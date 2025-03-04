767448b92abaf452608ab970fd955b9590e2fe59
imgonnatouchya <matteo200949@gmail.com>
2025-03-04 16:15:53 +0000

    dict changes

--- pokemon.py
+++ pokemon.py
@@ -3,18 +3,45 @@
 # by going like x[key], and then it'll retrieve the value of the key
 # keys can have identical values but values cannot have identical keys
 # vvv you can also nest them like so vvv
-pokemon = {"venusaur":{"type":0},
-           "charizard":{"type":0},
-           "blastoise":{"type":0},
-           "clefable":{"type":0},
-           "ditto":{"type":0},
-           "jolteon":{"type":0},
-           "flareon":{"type":0},
-           "porygon":{"type":0},
-           "snorlax":{"type":0},
-           "articuno":{"type":0},
-           "zapdos":{"type":0},
-           "moltres":{"type":0},
-           "dragonite":{"type":0},
-           "mewtwo":{"type":0}
+pokemon = {"venusaur":
+           {"type":{"type1":"grass", "type2":"poison"}},
+
+           "charizard":
+           {"type":{"type1":"flying", "type2":"fire"}},
+
+           "blastoise":
+           {"type":{"type1":"water", "type2":"none"}},
+
+           "clefable":
+           {"type":0},
+
+           "ditto":
+           {"type":0},
+
+           "jolteon":
+           {"type":0},
+
+           "flareon":
+           {"type":0},
+
+           "porygon":
+           {"type":0},
+
+           "snorlax":
+           {"type":0},
+
+           "articuno":
+           {"type":0},
+
+           "zapdos":
+           {"type":0},
+
+           "moltres":
+           {"type":0},
+
+           "dragonite":
+           {"type":0},
+
+           "mewtwo":
+           {"type":0}
            }
