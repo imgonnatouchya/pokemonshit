347a912244c87dcf3ed9e58b074555468cb05db5
imgonnatouchya <matteo200949@gmail.com>
2025-03-04 15:28:51 +0000

    Co-authored-by: ltmann <ltmann@users.noreply.github.com>

--- pokemon.py
+++ pokemon.py
@@ -1,1 +1,20 @@
-
+# pokemon dictionary, all info for pokemon which will be called later will be here
+# dictionaries come in the format: x = {key:value, key2:value2}, you call dictionaries
+# by going like x[key], and then it'll retrieve the value of the key
+# keys can have identical values but values cannot have identical keys
+# vvv you can also nest them like so vvv
+pokemon = {"venusaur":{"type":0},
+           "charizard":{"type":0},
+           "blastoise":{"type":0},
+           "clefable":{"type":0},
+           "ditto":{"type":0},
+           "jolteon":{"type":0},
+           "flareon":{"type":0},
+           "porygon":{"type":0},
+           "snorlax":{"type":0},
+           "articuno":{"type":0},
+           "zapdos":{"type":0},
+           "moltres":{"type":0},
+           "dragonite":{"type":0},
+           "mewtwo":{"type":0}
+           }
