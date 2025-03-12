50a5a7102a772ff2da40732053907ba52d91ff90
ltmann <86603951+ltmann@users.noreply.github.com>
2025-03-12 15:31:20 +0000

    more stuff

--- pokemon.py
+++ pokemon.py
@@ -4,6 +4,7 @@
 # keys can have identical values but values cannot have identical keys
 # vvv you can also nest them like so vvv
 import random
+import math
 pokemon = {"venusaur":
            
            {"type":{"type1":"grass", "type2":"poison"}},
@@ -66,34 +67,52 @@ pokemon = {"venusaur":
            {"type":{"type1":"psychic", "type2":"none"}}
            
            }
+#this is the list team assign randomizer as well as all the base stats set to the active slot(stats that will change when active changes)
+pokemon_keys = list(pokemon.keys())
 
-t1mon1 = random.choice(list(pokemon.items()))
-t1mon2 = random.choice(list(pokemon.items()))
-t1mon3 = random.choice(list(pokemon.items()))
-t1mon4 = random.choice(list(pokemon.items()))
-t1mon5 = random.choice(list(pokemon.items()))
-t1mon6 = random.choice(list(pokemon.items()))
+t1mon1 = random.choice(pokemon_keys)
+
+t1mon2 = random.choice(pokemon_keys)
+
+t1mon3 = random.choice(pokemon_keys)
+
+t1mon4 = random.choice(pokemon_keys)
+
+t1mon5 = random.choice(pokemon_keys)
+
+t1mon6 = random.choice(pokemon_keys)
+
 active = str(t1mon1)
+#base stats
 active_hp = 0
 active_atk = 0
 active_def = 0
 active_spa = 0
 active_spd = 0
 active_spe = 0
+#type
 active_type1 = "none"
 active_type2 = "none"
+#moves
 active_move1 = "none"
 active_move2 = "none"
 active_move3 = "none"
 active_move4 = "none"
+#ev's
 active_hp_evs = 0
 active_atk_evs = 0
 active_def_evs = 0
 active_spa_evs = 0
 active_spd_evs = 0
 active_spe_evs = 0
+#natures
 natures = ["adamant", "modest", "timid", "jolly", "bold", "calm", "brave", "serious"]
 active_nature = natures[7]
+nature_boost_atk = 1
+nature_boost_def = 1
+nature_boost_spa = 1
+nature_boost_spd = 1
+nature_boost_spe = 1
 
 print(active)
 pokemon_keys = list(pokemon.keys())
@@ -118,11 +137,7 @@ if active == pokemon_keys[1]:
     active_spd_evs = 0
     active_spe_evs = 252
     active_nature = natures[1]
-nature_boost_atk = 1
-nature_boost_def = 1
-nature_boost_spa = 1
-nature_boost_spd = 1
-nature_boost_spe = 1
+
 
 if active_nature == natures[0] or natures[6]:
     nature_boost_atk = 1.10
@@ -140,15 +155,16 @@ elif active_nature == natures[2] or natures[3]:
 
 #Hp calculator
 if active_hp_evs != 0:
-    print("hp is, ", ((31+2*active_hp+(active_hp_evs/4)+100)*50/100)+10)
+    print("hp is, ", (math.floor(((2*active_hp+31+(active_hp_evs/4))*50)/100)+50+10))
 else:
     print("parker likes dudes")
 if active_atk_evs != 0:
-    print("atk is,", ((31+2*active_atk+(active_atk_evs/4)*50/100+5)* nature_boost_atk))
+    print("atk is,", ((((2*active_atk+31+(active_atk_evs/4)*50)/100)+5)*nature_boost_atk))
 else:
     print("parker likes dudes")
+
 if active_spa_evs != 0:
-    print("spak is,", ((31+2*active_spa+(active_spa_evs/4)*50/100+5)* nature_boost_spa))
+    print("spak is,", math.floor((((2*active_spa+31+(active_spa_evs/4))*50)/100)+5)*nature_boost_spa))
 else:
     print("parker likes dudes")
 #print the list of the keys
